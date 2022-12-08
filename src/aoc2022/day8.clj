75bcd255f0b1836f843ec9e9e0690b7463cf140e
Laura Ketola <laura.ketola@gofore.com>
2022-12-08 13:33:54 +0200

    Day 8: Optimize

--- src/aoc2022/day8.clj
+++ src/aoc2022/day8.clj
@@ -2,32 +2,32 @@
   (:require [clojure.string :as str])
   (:require [aoc2022.util :as util]))
 
-(def transpose (partial apply map list))
+(defn transpose
+  [matrix]
+  (vec (apply map list matrix)))
 
 (defn parse-row
   [line]
-  (map #(Character/digit % 10) line))
+  (vec (map #(Character/digit % 10) line)))
 
 (defn is-visible
-  [row x]
-  (let [val (nth row x)]
-    (not (and
-          (some #(>= % val) (take x row))
-          (some #(>= % val) (drop (inc x) row))))))
+  [row rowr x]
+  (let [val (nth row x)
+        watch #(some (partial < val) (take x %))]
+    (not (and (watch row) (watch rowr)))))
 
 (defn can-see
   [stack height n]
   (if (empty? stack) n
       (let [n (inc n)]
-        (if (>= (first stack) height)
-          n
-          (recur (rest stack) height n)))))
+        (if (>= (first stack) height) n
+            (recur (rest stack) height n)))))
 
 (defn scenic-score
-  [row x]
-  (let [val (nth row x)]
-    (* (can-see (take x row) val 0)
-       (can-see (reverse (drop (inc x) row)) val 0))))
+  [row rowr x]
+  (let [val (nth row x)
+        watch #(can-see (take x %) val 0)]
+    (* (watch row) (watch rowr))))
 
 (defn matrix-loop
   [fun combiner data]
@@ -35,15 +35,17 @@
     (for [x (range 0 (count (first data)))
           y (range 0 (count data))]
       (let [row (nth data y)
-            col (nth cols x)]
-        (combiner (fun row x) (fun col y))))))
+            rowr (reverse row)
+            col (nth cols x)
+            colr (reverse col)]
+        (future (combiner (fun row rowr x) (fun col colr y)))))))
 
 (defn part1
   [filename]
   (->> filename util/load-input
        (map parse-row)
        (matrix-loop is-visible #(or %1 %2))
-       (filter identity)
+       (filter deref)
        count))
 
 (defn part2
@@ -51,6 +53,7 @@
   (->> filename util/load-input
        (map parse-row)
        (matrix-loop scenic-score *)
+       (map deref)
        (apply max)))
 
 (assert (part1 "day8_example") 21)
