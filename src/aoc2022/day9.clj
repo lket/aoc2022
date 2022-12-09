03b53fb3bad7fbff0734318d73160de92e2ed676
Laura Ketola <laura.ketola@gofore.com>
2022-12-09 14:16:14 +0200

    Day 9: Simplify movement

--- src/aoc2022/day9.clj
+++ src/aoc2022/day9.clj
@@ -2,7 +2,7 @@
   (:require [clojure.string :as str])
   (:require [aoc2022.util :as util]))
 
-(def state
+(def state1
   {:head {:x 0 :y 0}
    :tail [{:x 0 :y 0}]
    :visited #{{:x 0 :y 0}}})
@@ -17,18 +17,16 @@
   (let [[direction num] (str/split line #" ")
         n (util/str->int num)]
     (repeat n (case direction
-                "R" :right
-                "L" :left
-                "U" :up
-                "D" :down))))
+                "R" [1 0]
+                "L" [-1 0]
+                "U" [0 1]
+                "D" [0 -1]))))
 
 (defn move
-  [state dir key]
-  (case dir
-    :right (update-in state [key :x] inc)
-    :left (update-in state [key :x] dec)
-    :up (update-in state [key :y] inc)
-    :down (update-in state [key :y] dec)))
+  [state key [x y]]
+  (-> state
+      (update-in (conj key :x) #(+ % x))
+      (update-in (conj key :y) #(+ % y))))
 
 (defn cap
   [x max]
@@ -50,42 +48,42 @@
         xdif (- (:x head) (:x tail))
         ydif (- (:y head) (:y tail))
         [xmove ymove] (needs-to-move xdif ydif)]
-    (-> state
-        (update-in (conj tailpath :x) #(+ % xmove))
-        (update-in (conj tailpath :y) #(+ % ymove)))))
+    (move state tailpath [xmove ymove])))
 
 (defn add-visited
-  [state tail]
-  (update-in state [:visited] #(conj % (get-in state tail))))
+  [state]
+  (update-in state [:visited] #(conj % (last (get state :tail)))))
 
 (defn handle-movement
   [state dir]
-  (let [headmoved (move state dir :head)
-        tailmoved (reduce (fn [state tail-n]
-                            (let [follow (if (zero? tail-n) [:head] [:tail (dec tail-n)])]
-                              (tail-follow state follow [:tail tail-n])))
-                          headmoved
-                          (range 0 (count (:tail headmoved))))]
-    (add-visited tailmoved [:tail 8])))
+  (let [head-moved (move state [:head] dir)
+        head-tail-list
+        (partition 2 1 (conj (map-indexed (fn [i _] [:tail i]) (:tail head-moved))
+                        [:head]))
+        tails-moved (reduce (fn [state [head tail]]
+                             (tail-follow state head tail))
+                           head-moved
+                           head-tail-list)]
+    (add-visited tails-moved)))
 
-(defn part1
-  [filename]
+(defn solve
+  [filename state]
   (->> filename util/load-input
        (mapcat parse-movement)
        (reduce handle-movement state)
        :visited
        count))
 
+(defn part1
+  [filename]
+  (solve filename state1))
+
 (defn part2
   [filename]
-  (->> filename util/load-input
-       (mapcat parse-movement)
-       (reduce handle-movement state2)
-       :visited
-       count))
+  (solve filename state2))
 
-(assert (part1 "day9_input") 5930)
-(assert (part1 "day9_example") 13)
-(assert (part2 "day9_example") 1)
-(assert (part2 "day9_example_long") 36)
-(assert (part2 "day9_input") 2443)
+(assert (= (part1 "day9_input") 5930))
+(assert (= (part1 "day9_example") 13))
+(assert (= (part2 "day9_example") 1))
+(assert (= (part2 "day9_example_long") 36))
+(assert (= (part2 "day9_input") 2443))
