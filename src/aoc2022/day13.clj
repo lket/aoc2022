1dbf2342d5ebfbc1066ba7dd4718f43697ffe6ca
Laura Ketola <laura.ketola@gofore.com>
2022-12-13 16:39:15 +0200

    Day 13

--- src/aoc2022/day13.clj
+++ src/aoc2022/day13.clj
@@ -3,52 +3,52 @@
             [aoc2022.util :as util]))
 
 (defn check-order
-  [items]
-  (if (seq items) 
-    (let [[i1 i2] (peek items)
-          remaining (if (seq items) (pop items) nil)]
-      (println i1 i2)
-      (cond
-        (= i1 i2 ##-Inf) (recur remaining)
-        (= i1 ##-Inf) true
-        (= i2 ##-Inf) false
-        (or (seqable? i1) (seqable? i2))
-        (let [i1 (if (seqable? i1) (if (seq i1) i1 [##-Inf]) [i1 ##-Inf])
-              i2 (if (seqable? i2) (if (seq i2) i2 [##-Inf]) [i2 ##-Inf])
-              inner-result (check-order (vec (rseq (mapv #(vector %1 %2) i1 i2))))]
-          (if (= inner-result :end-reached) (recur remaining) inner-result))
-        (< i1 i2) true
-        (= i1 i2) (recur remaining)
-        (> i1 i2) false)) :end-reached))
-
-
-(check-order '((1 1)))
+  [p1 p2]
+  (let [i1 (peek p1)
+        i2 (peek p2)
+        r1 (if (seq p1) (pop p1) nil)
+        r2 (if (seq p2) (pop p2) nil)]
+    (cond
+      (= i1 i2 nil) :end-reached
+      (nil? i1) true
+      (nil? i2) false
+      (or (seqable? i1) (seqable? i2))
+      (let [i1 (if (seqable? i1) (vec (rseq i1)) [i1])
+            i2 (if (seqable? i2) (vec (rseq i2)) [i2])
+            inner-result (check-order i1 i2)]
+        (if (= inner-result :end-reached) (recur r1 r2) inner-result))
+      (< i1 i2) true
+      (= i1 i2) (recur r1 r2)
+      (> i1 i2) false)))
 
 (defn part1
   [filename]
   (->> filename util/load-input
        (remove str/blank?)
        (map read-string)
-       (map seq)
+       (map rseq)
+       (map vec)
        (partition 2)
-       (mapv (fn [[p1 p2]] (mapv #(vector %1 %2) (concat p1 [##-Inf]) (concat p2 [##-Inf]))))
-       (mapv (comp vec rseq))
-       (map check-order)
+       (map (fn [[l1 l2]] (check-order l1 l2)))
        (map #(vector (inc %1) %2) (range))
        (filter (fn [[_ val]] (= true val)))
        (map first)
        (reduce +)))
 
+(defn part2
+  [filename]
+  (->> filename util/load-input
+       (remove str/blank?)
+       (map read-string)
+       (map rseq)
+       (map vec)
+       (concat [[[2]] [[6]]])
+       (sort check-order)
+       (map #(vector (inc %1) %2) (range))
+       (reduce (fn [r [i v]] (if (or (= v [[6]]) (= v [[2]])) (conj r i) r)) [])
+       (apply *)))
 
-(part1 "day13_example")
-(part1 "day13_input")
-(part1 "day13_test")
-(seqable? [[]])
-(seq? [])
-
-
-(concat [1 2] '(1 3))
-
-(mapv #(vector %1 %2) (rseq [2 3 4]) [4 ##-Inf])
-(mapv #(vector %1 %2) (rseq [1 2 3]) [5 4])
-(mapv #(vector %1 %2) [[]] [##-Inf])
+(assert (= (part1 "day13_example") 13))
+(assert (= (part1 "day13_input") 5720))
+(assert (= (part2 "day13_example") 140))
+(assert (= (part2 "day13_input") 23504))
