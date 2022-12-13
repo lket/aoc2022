7e9e4acb7fed5efa8a38425048b3c1c501a34d12
Laura Ketola <laura.ketola@gofore.com>
2022-12-13 21:17:00 +0200

    Day 13: Don't reverse vecs

--- src/aoc2022/day13.clj
+++ src/aoc2022/day13.clj
@@ -5,37 +5,29 @@
 
 (defmacro check-list
   [i1 i2 r1 r2]
-  `(let [p1# (vec (rseq ~i1))
-         p2# (vec (rseq ~i2))
-         inner-result# (check-order p1# p2#)]
+  `(let [inner-result# (check-order ~i1 ~i2)]
      (if (= inner-result# :end-reached)
        (recur ~r1 ~r2)
        inner-result#)))
 
 (defn check-order
-  [p1 p2]
-  (let [i1 (peek p1)
-        i2 (peek p2)
-        r1 (if i1 (pop p1) nil)
-        r2 (if i2 (pop p2) nil)]
-    (match [i1 i2]
-      [nil   nil  ] :end-reached
-      [nil   _    ] true
-      [_     nil  ] false
-      [[& _] [& _]] (check-list i1 i2 r1 r2)
-      [_     [& _]] (check-list [i1] i2 r1 r2)
-      [[& _] _    ] (check-list i1 [i2] r1 r2)
-      [_     _    ] (cond (< i1 i2) true
-                          (> i1 i2) false
-                          (= i1 i2) (recur r1 r2)))))
+  [[i1 & r1] [i2 & r2]]
+  (match [i1 i2]
+    [nil   nil  ] :end-reached
+    [nil   _    ] true
+    [_     nil  ] false
+    [[& _] [& _]] (check-list i1 i2 r1 r2)
+    [_     [& _]] (check-list [i1] i2 r1 r2)
+    [[& _] _    ] (check-list i1 [i2] r1 r2)
+    [_     _    ] (cond (< i1 i2) true
+                        (> i1 i2) false
+                        (= i1 i2) (recur r1 r2))))
 
 (defn part1
   [filename]
   (->> filename util/load-input
        (remove str/blank?)
        (map read-string)
-       (map rseq)
-       (map vec)
        (partition 2)
        (map (fn [[l1 l2]] (check-order l1 l2)))
        (keep-indexed (fn [i val] (when val (inc i))))
@@ -46,8 +38,6 @@
   (->> filename util/load-input
        (remove str/blank?)
        (map read-string)
-       (map rseq)
-       (map vec)
        (concat [[[2]] [[6]]])
        (sort check-order)
        (keep-indexed (fn [i val] (when (some #(= val %) [[[2]] [[6]]]) (inc i))))
@@ -57,3 +47,4 @@
 (assert (= (part1 "day13_input") 5720))
 (assert (= (part2 "day13_example") 140))
 (assert (= (part2 "day13_input") 23504))
+
