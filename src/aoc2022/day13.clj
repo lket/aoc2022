02f975c3551202eca00c3448f20b6deb1de85b1a
Laura Ketola <laura.ketola@gofore.com>
2022-12-13 20:53:08 +0200

    Day 13: Use core.match

--- src/aoc2022/day13.clj
+++ src/aoc2022/day13.clj
@@ -1,25 +1,33 @@
 (ns aoc2022.day13
   (:require [clojure.string :as str]
+            [clojure.core.match :refer [match]]
             [aoc2022.util :as util]))
+
+(defmacro check-list
+  [i1 i2 r1 r2]
+  `(let [p1# (vec (rseq ~i1))
+         p2# (vec (rseq ~i2))
+         inner-result# (check-order p1# p2#)]
+     (if (= inner-result# :end-reached)
+       (recur ~r1 ~r2)
+       inner-result#)))
 
 (defn check-order
   [p1 p2]
   (let [i1 (peek p1)
         i2 (peek p2)
-        r1 (if (seq p1) (pop p1) nil)
-        r2 (if (seq p2) (pop p2) nil)]
-    (cond
-      (= i1 i2 nil) :end-reached
-      (nil? i1) true
-      (nil? i2) false
-      (or (seqable? i1) (seqable? i2))
-      (let [i1 (if (seqable? i1) (vec (rseq i1)) [i1])
-            i2 (if (seqable? i2) (vec (rseq i2)) [i2])
-            inner-result (check-order i1 i2)]
-        (if (= inner-result :end-reached) (recur r1 r2) inner-result))
-      (< i1 i2) true
-      (= i1 i2) (recur r1 r2)
-      (> i1 i2) false)))
+        r1 (if i1 (pop p1) nil)
+        r2 (if i2 (pop p2) nil)]
+    (match [i1 i2]
+      [nil   nil  ] :end-reached
+      [nil   _    ] true
+      [_     nil  ] false
+      [[& _] [& _]] (check-list i1 i2 r1 r2)
+      [_     [& _]] (check-list [i1] i2 r1 r2)
+      [[& _] _    ] (check-list i1 [i2] r1 r2)
+      [_     _    ] (cond (< i1 i2) true
+                          (> i1 i2) false
+                          (= i1 i2) (recur r1 r2)))))
 
 (defn part1
   [filename]
@@ -30,9 +38,7 @@
        (map vec)
        (partition 2)
        (map (fn [[l1 l2]] (check-order l1 l2)))
-       (map #(vector (inc %1) %2) (range))
-       (filter (fn [[_ val]] (= true val)))
-       (map first)
+       (keep-indexed (fn [i val] (when val (inc i))))
        (reduce +)))
 
 (defn part2
@@ -44,8 +50,7 @@
        (map vec)
        (concat [[[2]] [[6]]])
        (sort check-order)
-       (map #(vector (inc %1) %2) (range))
-       (reduce (fn [r [i v]] (if (or (= v [[6]]) (= v [[2]])) (conj r i) r)) [])
+       (keep-indexed (fn [i val] (when (some #(= val %) [[[2]] [[6]]]) (inc i))))
        (apply *)))
 
 (assert (= (part1 "day13_example") 13))
